c56cdcc38678817f71d76241bcec3da2d3962360
B9V9Z9 <b9v9z9@gmail.com>
2022-12-25 12:13:58 +0100

    Nieuwe update

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Board():
         self.x = 0
         self.y = 0
         self.size = size
+        self.possible_moves = {}
 
         self.SCALE = 150
         if size == 6:
@@ -111,65 +112,76 @@ class Board():
             for j in range(0, len(self.grid)):
                 self.place_disc(i, j, True)
 
-    def place_disc(self, h_pos, v_pos, help=False):
-        # Puting The Logic behind placing a disc
+    def update_possible_moves(self):
+        for i in range(0, len(self.grid)):
+            for j in range(0, len(self.grid)):
+                self.place_disc(i, j, True)
+
+    def pieces_to_flip(self, h_pos, v_pos):
+        # Returns pieces to be flipped with current move
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
         pieces_to_flip = []
-        try:
-            if self.grid[h_pos][v_pos] == self.EMPTY:
-                # One Method for All Directions
-                for direction in directions:
-                    current_position = [h_pos + direction[0], v_pos + direction[1]]
-                    temporary_pieces_to_flip = []
-                    try:
-                        while True:
-                            if current_position[0] < 0 or current_position[1] < 0:
-                                break
-                            # Als je aan het einde jezelf tegen komt, dan wordt de temp lijst van die ENE richting toegevoegd aan de echte lijst
-                            if self.grid[current_position[0]][current_position[1]] == self.active_player:
-                                for piece in temporary_pieces_to_flip:
-                                    pieces_to_flip.append(piece)
-                                break
-                            elif self.grid[current_position[0]][current_position[1]] != self.active_player and \
-                                    self.grid[current_position[0]][current_position[1]] != self.EMPTY:
-                                temporary_pieces_to_flip.append(current_position.copy())
-                                # Verder in huidige richting en vijand toevoegen aan temporary pieces to flip lijst
-                                current_position[0] += direction[0]
-                                current_position[1] += direction[1]
-                                # Continue zodat de loop doorgaat in dezelfde richting als de loop een vijand vindt.
-                                continue
-                            else:
-                                break
-                    # Als de functie uit ge grid breekt dan stopt ie en gaat hij door naar de volgende richting
-                    except IndexError:
-                        print("oeps")
-
-                if len(pieces_to_flip) > 0 or self.drawing_board:
-                    # if the help button is pressed, grey outlined circles will show legal moves for the current player
-                    if help:
-                        self.draw.ellipse(((h_pos * self.SCALE + 5, v_pos * self.SCALE + 5),
-                                           (h_pos * self.SCALE + self.SCALE - 5, v_pos * self.SCALE + self.SCALE - 5)),
-                                          outline="grey", width=4)
-                    # If the help button isn't pressed, then just draw the disc on the legal location
-                    else:
-                        # The coordinate of the mouse is transferred to the grid, transforming the "empty" value into a player value
-                        self.grid[h_pos][v_pos] = self.active_player
-                        self.draw.ellipse(((h_pos * self.SCALE + 5, v_pos * self.SCALE + 5),
-                                           (h_pos * self.SCALE + self.SCALE - 5, v_pos * self.SCALE + self.SCALE - 5)),
-                                          self.active_player)
-                        # For every piece that was registered that had to be flipped, flip their value in the grid and color in the GUI
-                        for piece in pieces_to_flip:
-                            self.grid[piece[0]][piece[1]] = self.active_player
-                            self.draw.ellipse(((piece[0] * self.SCALE + 5, piece[1] * self.SCALE + 5), (
-                            piece[0] * self.SCALE + self.SCALE - 5, piece[1] * self.SCALE + self.SCALE - 5)),
-                                              self.active_player)
-                        # If player was black, next player becomes white, and vice versa
-                        if self.active_player == self.BLACK:
-                            self.active_player = self.WHITE
+        if self.grid[h_pos][v_pos] == self.EMPTY:
+            # One Method for All Directions
+            for direction in directions:
+                current_position = [h_pos + direction[0], v_pos + direction[1]]
+                temporary_pieces_to_flip = []
+                try:
+                    while True:
+                        if current_position[0] < 0 or current_position[1] < 0:
+                            break
+                        # Als je aan het einde jezelf tegen komt, dan wordt de temp lijst van die ENE richting toegevoegd aan de echte lijst
+                        if self.grid[current_position[0]][current_position[1]] == self.active_player:
+                            for piece in temporary_pieces_to_flip:
+                                pieces_to_flip.append(piece)
+                            break
+                        elif self.grid[current_position[0]][current_position[1]] != self.active_player and \
+                                self.grid[current_position[0]][current_position[1]] != self.EMPTY:
+                            temporary_pieces_to_flip.append(current_position.copy())
+                            # Verder in huidige richting en vijand toevoegen aan temporary pieces to flip lijst
+                            current_position[0] += direction[0]
+                            current_position[1] += direction[1]
+                            # Continue zodat de loop doorgaat in dezelfde richting als de loop een vijand vindt.
+                            continue
                         else:
-                            self.active_player = self.BLACK
-                        self.score()
-                    self.update_playing_field()
+                            break
+                # Als de functie uit ge grid breekt dan stopt ie en gaat hij door naar de volgende richting
+                except IndexError:
+                    print("oeps")
+        return pieces_to_flip
+
+    def place_disc(self, h_pos, v_pos, help=False):
+        # Puting The Logic behind placing a disc
+
+        try:
+
+
+            if len(pieces_to_flip) > 0 or self.drawing_board:
+                # if the help button is pressed, grey outlined circles will show legal moves for the current player
+                if help:
+                    self.draw.ellipse(((h_pos * self.SCALE + 5, v_pos * self.SCALE + 5),
+                                       (h_pos * self.SCALE + self.SCALE - 5, v_pos * self.SCALE + self.SCALE - 5)),
+                                      outline="grey", width=4)
+                # If the help button isn't pressed, then just draw the disc on the legal location
+                else:
+                    # The coordinate of the mouse is transferred to the grid, transforming the "empty" value into a player value
+                    self.grid[h_pos][v_pos] = self.active_player
+                    self.draw.ellipse(((h_pos * self.SCALE + 5, v_pos * self.SCALE + 5),
+                                       (h_pos * self.SCALE + self.SCALE - 5, v_pos * self.SCALE + self.SCALE - 5)),
+                                      self.active_player)
+                    # For every piece that was registered that had to be flipped, flip their value in the grid and color in the GUI
+                    for piece in pieces_to_flip:
+                        self.grid[piece[0]][piece[1]] = self.active_player
+                        self.draw.ellipse(((piece[0] * self.SCALE + 5, piece[1] * self.SCALE + 5), (
+                        piece[0] * self.SCALE + self.SCALE - 5, piece[1] * self.SCALE + self.SCALE - 5)),
+                                          self.active_player)
+                    # If player was black, next player becomes white, and vice versa
+                    if self.active_player == self.BLACK:
+                        self.active_player = self.WHITE
+                    else:
+                        self.active_player = self.BLACK
+                    self.score()
+                self.update_playing_field()
 
         except IndexError:
             print("Can't place here.")
